ccf5d5ab54532a02aabbe82363a4d5b458333b5e
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-04-23 05:03:56 +0900

    The example is meant to be used in a pipe...

--- le/sng_jisoo.ts
+++ le/sng_jisoo.ts
@@ -1,16 +1,13 @@
 import * as readline from "node:readline";
-import { stdin, stdout } from "node:process";
+import { stdin } from "node:process";
 
 import { Ku } from "../src/index.ts";
 
 async function sng() {
   const rl = readline.createInterface(stdin);
-  const prompt = "算字數：";
-  stdout.write(prompt);
   for await (const line of rl) {
     let jisoo = [...new Ku(line.trimEnd()).thianji()].length;
     console.log(`字數=${jisoo}`);
-    stdout.write(prompt);
   }
 }
 
