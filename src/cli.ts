25907c1c9ea6d8c71367681a51af55507906bc1e
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-05-29 21:15:09 +0900

    handle command cases and sort of implement syllable count

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,21 @@
 import { Ku } from "./index.ts";
+import * as readline from "node:readline";
 import { parseArgs } from "node:util";
 
-function main() {
+function err(msg: string) {
+  console.error(msg);
+  process.exit(1);
+}
+
+async function main() {
   const parsedArgs = parseArgs({
     args: process.argv.slice(2),
-    allowPositionals: true,
     options: {
       help: { type: "boolean", short: "h" },
+      input: { type: "string", short: "i" },
+      output: { type: "string", short: "o" },
+      to: { type: "string" },
+      count: { type: "boolean" },
     },
   });
   if (parsedArgs.values.help) {
@@ -22,6 +31,27 @@ Options:
 `);
     process.exit(0);
   }
+  if (parsedArgs.values.to) {
+    if (parsedArgs.values.count) {
+      err("Please only specify either --to <kip|poj> or --count");
+    }
+    if (!["kip", "poj", "tl"].includes(parsedArgs.values.to!)) {
+      err('--to needs to be either "kip" or "poj"');
+    }
+    const to = parsedArgs.values.to as "kip" | "poj" | "tl";
+  } else if (parsedArgs.values.count) {
+    if (!parsedArgs.values.to && !parsedArgs.values.count) {
+      err("Either --to <kip|poj> or --count has to be specified");
+    }
+    const rl = readline.createInterface(process.stdin);
+    let count = 0;
+    for await (const line of rl) {
+      count += [...new Ku(line.trimEnd()).thianji()].length;
+    }
+    console.log(count);
+  } else {
+    err("Either --to <kip|poj> or --count has to be specified");
+  }
 }
 
-main();
+await main();
